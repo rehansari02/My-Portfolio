1863f9888dce1a502131ef85e66976c40ff61bc6
Ansari Rehan <rehansari798@gmail.com>
2025-09-30 17:44:09 +0530

    Update About.jsx
    Update Resume link

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -47,7 +47,7 @@ const About = () => {
 
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1NRAQ_Sh8WDkDdvFrwC5AgUA20Ht0QeNA/view"
+            href="https://drive.google.com/file/d/175UCvWFUORJGWpvANnLVGboQx4eHqw3y/view?usp=drive_link"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
