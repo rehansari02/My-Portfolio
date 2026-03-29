a39d4d50b95fb83535554ff4af97b8fd31b5b20f
rehansari02 <rehansari798@gmail.com>
2026-03-30 01:05:50 +0530

    update resume link

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/175UCvWFUORJGWpvANnLVGboQx4eHqw3y/view?usp=drive_link"
+            href="https://drive.google.com/file/d/1vfzLkOJjkyILJqJBxe1EHehFH56YURBb/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
