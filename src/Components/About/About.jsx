13af4e9cf936603536d7356ee2e0a3efcda09152
rehansari02 <rehansari798@gmail.com>
2026-07-05 01:46:45 +0530

    fix: update resume link in About section

--- src/Components/About/About.jsx
+++ src/Components/About/About.jsx
@@ -56,7 +56,7 @@ const About = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1vfzLkOJjkyILJqJBxe1EHehFH56YURBb/view?usp=sharing"
+            href="https://drive.google.com/file/d/1QAoV__e0yWgHHgH3EKDO-WYSK0bWMYxW/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
